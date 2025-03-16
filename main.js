bc2ee19c6291de398a9f0f98a13fed7ba2fb54ff
caiocroccia22 <caiocroccia22@gmail.com>
2025-03-16 12:06:08 -0300

    chore: import JQuery

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
-document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('btn-buscar-cep').addEventListener('click', function(){
-        // AJAXX - Asynchronous JavaScript and XML
+// document.addEventListener('DOMContentLoaded', function(){
+//     document.getElementById('btn-buscar-cep').addEventListener('click', function(){
+     // AJAXX - Asynchronous JavaScript and XML
         // Interação entre o front end e o back end sem precisar recarregar a página
         // Requisição HTTP para o servidor
-        const xhttp = new XMLHttpRequest();
-        const cep = document.getElementById('cep').value;
-        const endpoint = `https://viacep.com.br/ws/${cep}/json/`;
+//         const xhttp = new XMLHttpRequest();
+//         const cep = document.getElementById('cep').value;
+//         const endpoint = `https://viacep.com.br/ws/${cep}/json/`;
 
-        xhttp.open('GET', endpoint);
-        xhttp.send();
+//         xhttp.open('GET', endpoint);
+//         xhttp.send();
 
         
-    })
-})
+//     })
+// })
 
-$document.ready(function(){
+$(document).ready(function(){
     $('#btn-buscar-cep').click(function() {
         const cep = $('#cep').val();
+        const endpoint = `https://viacep.com.br/ws/${cep}/json/`;
+        // parametro resposta é o retorno da requisição
+        $.ajax(endpoint).done(function(resposta) {
+            console.log(resposta);
+        })
     })
 })
