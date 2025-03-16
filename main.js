7866431c0c413f908e995745fc0ee33710dcdada
caiocroccia22 <caiocroccia22@gmail.com>
2025-03-16 12:44:12 -0300

    feat: add jquery mask plugin for cep input and loading button

--- main.js
+++ main.js
@@ -15,12 +15,51 @@
 // })
 
 $(document).ready(function(){
+    $('#cep').mask('00000-000');
+
     $('#btn-buscar-cep').click(function() {
         const cep = $('#cep').val();
         const endpoint = `https://viacep.com.br/ws/${cep}/json/`;
+        const botao = $(this);
+
+        // this é o botão que foi clicado, ou seja #btn-buscar-cep
+        // $(this) é um objeto jQuery que representa o botão clicado
+        botao.find('i').addClass('d-none');
+        botao.find('span').removeClass('d-none');
+
         // parametro resposta é o retorno da requisição
         $.ajax(endpoint).done(function(resposta) {
-            console.log(resposta);
+            // console.log(resposta);
+            const logradourou = resposta.logradouro;
+            const bairro = resposta.bairro;
+            const localidade = resposta.localidade;
+            const estado = resposta.uf;
+            const endereco = `${logradourou}, ${bairro}, ${localidade}, ${estado}`;
+            $('#endereco').val(endereco);
+
+            setTimeout(function(){
+                botao.find('i').removeClass('d-none');
+                botao.find('span').addClass('d-none');
+            }, 4000);
         })
     })
 })
+
+
+// Resposta JSON
+
+// {
+//     "cep": "11045-201",
+//     "logradouro": "Avenida Siqueira Campos",
+//     "complemento": "de 333 ao fim - lado ímpar",
+//     "unidade": "",
+//     "bairro": "Embaré",
+//     "localidade": "Santos",
+//     "uf": "SP",
+//     "estado": "São Paulo",
+//     "regiao": "Sudeste",
+//     "ibge": "3548500",
+//     "gia": "6336",
+//     "ddd": "13",
+//     "siafi": "7071"
+// }
